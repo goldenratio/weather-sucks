67b76e09be1db6d6250fbc90d80c939c1a8f6d5a
kuuuurija <karthikeyanvj@gmail.com>
2018-12-27 15:57:29 +0200

    updated fetch callback

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,5 @@
+const version = "0.1.0";
+const cacheName = `weather-sucks-${version}`;
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
@@ -10,5 +12,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
