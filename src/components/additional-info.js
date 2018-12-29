a7daf3e23d3908539cf406a138e3925b9d861b67
kuuuurija <karthikeyanvj@gmail.com>
2018-12-29 13:08:38 +0200

    cleanup additional info

--- src/components/additional-info.js
+++ src/components/additional-info.js
@@ -4,5 +4,5 @@ import { html } from '../libs/preact.js';
  * @param {AdditionalInfoProps} props
  */
 export const AdditionalInfo = ({}) => {
-  return html`<div>Additional Info</div>`;
+  return html`<div></div>`;
 };
