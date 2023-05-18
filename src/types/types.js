9d740540ee4cd3b8f1cc8ede2c0aa161b3e45d5d
karthikeyan.jayakuma <karthikeyan.jayakumar@gamesys.ee>
2023-05-18 11:00:02 +0300

    minor cleanup

--- src/types/types.js
+++ src/types/types.js
@@ -67,3 +67,37 @@
  * @property {number} windDirection in degrees
  * @property {number} visibility in meters
  */
+
+/**
+ * @typedef {function} CloseCallback
+ * @function
+ * @return {void}
+ */
+
+/**
+ * @typedef {function} SaveClickCallback
+ * @function
+ * @param {string} city
+ * @param {Unit} unit
+ * @return {void}
+ */
+
+/**
+ * @typedef {object} SettingsPanelProps
+ * @property {CloseCallback} onCloseClick
+ * @property {SaveClickCallback} onSaveClick
+ * @property {string} city
+ * @property {Unit} unit
+ */
+
+/**
+ * @typedef {object} SettingsPanelState
+ * @property {string} [city]
+ * @property {Unit} [unit]
+ */
+
+/**
+ * @typedef {object} AdditionalInfoProps
+ * @property {boolean} show
+ * @property {AdditionalInfo} data
+ */
